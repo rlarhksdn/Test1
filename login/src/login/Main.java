f2ecb3ec18b639f24fa9d3fb75705d3e25d6e19e
kimgwanwoo <kimgwanwoo@DESKTOP-VPURRK3>
2022-08-10 16:36:08 +0900

    두번째 버전
    최초시작히 소개화면 추가

--- login/src/login/Main.java
+++ login/src/login/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
+		System.out.println("로그인을 시작하지");
 		String id,pw,tel;		
 		Check check = new Check();
 		Check del = new Check();
